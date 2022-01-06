00eee5c2f18e6d2a033f1458283802165fd26012
joey <doode1524@gmail.com>
2022-01-05 22:05:52 -0500

    remove div and refactor app styling to index.css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import { LetterContainer } from "./gameboard/components/LetterContainer";
 
 const App = () => {
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <h1>WORDLE</h1>
+    <>
+      <h1 className="app-header">WORDLE</h1>
       <LetterContainer>Hello</LetterContainer>
-    </div>
+    </>
   );
 };
 
