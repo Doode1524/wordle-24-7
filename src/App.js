fa86184e4c838cea3ee4c3e5d78a8d19b189c451
joey <doode1524@gmail.com>
2022-01-22 20:39:23 -0500

    fetch curWord

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/form";
 import { LetterContainer } from "./components/gameboard/LetterContainer";
 
 const App = () => {
-  const curWord = "REACT";
+  const [curWord, setCurWord] = useState("");
   const [word, setWord] = useState(["", "", "", "", "", "", ""]);
   const [curRow, setCurRow] = useState(0);
 
   let form = document.getElementById("word-form");
+
+  const fetchWord = () => {
+    fetch("https://random-word-api.herokuapp.com/word?number=10")
+      .then((res) => res.json())
+      .then((data) => {
+        let newWord = data.find((word) => word.length === 5);
+        if (!newWord) {
+          fetchWord();
+        } else {
+          setCurWord(newWord.toUpperCase());     
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetchWord();
+  }, []);
 
   const handleChange = (e) => {
     let newWord = [...word];
@@ -29,12 +46,11 @@ const App = () => {
     let rowBoxes = Array.from(letterRow.children).map((box) => {
       return box;
     });
-
-    enteredWordArr.forEach((letter, i) => {  
+    enteredWordArr.forEach((letter, i) => {
       rowBoxes.forEach((box) => {
         let letterCheck = wordArr.find((element) => box.innerText === element);
         let boxEle = rowBoxes.find((box) => box.id === `box-${i + 1}`);
-        
+
         if (letter === wordArr[i]) {
           boxEle.style.backgroundColor = "#6aaa64";
           boxEle.style.color = "white";
