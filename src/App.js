92ca4ae8176d083e3a2565bf5f05fd7179dab8f1
joey <doode1524@gmail.com>
2022-01-25 22:52:40 -0500

    bug 2 note

--- src/App.js
+++ src/App.js
@@ -48,6 +48,8 @@ const App = () => {
     // if you enter two of the same letter and one of them is in the 
     // correct spot, then the block will permanently be yellow 
     // no matter what letter you enter
+
+    // found second bug. Same as above, but if you guess the last letter correctly
     let enteredWordArr = enteredWord.split("");
     let wordArr = curWord.split("");
     let letterRow = document.getElementById(`row-${curRow + 1}`);
