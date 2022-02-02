4c18c434bf6b7b2e1a1bf4bf20b38f4d23dc74f1
joey <doode1524@gmail.com>
2022-02-01 22:31:31 -0500

    fix yellow bug, just duplicates left

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ const App = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (isValidGuess(word[curRow])) {
-      checkWord(word[curRow]);
       setCurRow(curRow + 1);
+      checkWord(word[curRow]);
       form.reset();
     }
   };
@@ -55,37 +55,22 @@ const App = () => {
   };
 
   const checkWord = (enteredWord) => {
-    // TO RECREATE BUG //
-    // if you enter two of the same letter and one of them is in the
-    // correct spot, then the block will permanently be yellow
-    // no matter what letter you enter
-
-    // found second bug. Same as above, but if you guess the last letter correctly
-
-    let enteredWordArr = enteredWord.split("");
-    let wordArr = curWord.split("");
     let letterRow = document.getElementById(`row-${curRow + 1}`);
     let rowBoxes = Array.from(letterRow.children).map((box) => {
       return box;
     });
 
-    enteredWordArr.forEach((letter, i) => {
-      rowBoxes.forEach((box) => {
-        let letterCheck = wordArr.find((element) => box.innerText === element);
-        let boxEle = rowBoxes.find((box) => box.id === `box-${i + 1}`);
-
-        if (letter === wordArr[i]) {
-          boxEle.style.backgroundColor = "#6aaa64"; //green
-          boxEle.style.color = "white";
-        } else if (letterCheck !== undefined) {
-          letterCheck = undefined;
-          boxEle.style.backgroundColor = "#c9b458"; //yellow
-          boxEle.style.color = "white";
-        } else {
-          box.style.backgroundColor = "#787c7e"; //black
-          box.style.color = "white";
-        }
-      });
+    enteredWord.split("").map((letter, i) => {
+      if (letter == curWord[i]) {
+        rowBoxes[i].style.backgroundColor = "#6aaa64";
+        rowBoxes[i].style.color = "white";
+      } else if (curWord.split("").includes(letter)) {
+        rowBoxes[i].style.backgroundColor = "#c9b458";
+        rowBoxes[i].style.color = "white";
+      } else {
+        rowBoxes[i].style.backgroundColor = "#787c7e";
+        rowBoxes[i].style.color = "white";
+      }
     });
   };
 
